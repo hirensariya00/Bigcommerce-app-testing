9fce4e4533b8b451df9ff19d1c22102a0a46a843
hirensariya00 <hiren.sariya@rapidops.com>
2023-08-11 17:57:53 +0530

    first commit.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <input type="text" name="Enter Name" placeholder="Enter Name" />
+          <input type="text" name="Enter Email" placeholder="Enter Email" />
+          <input type="text" name="Enter Password" placeholder="Enter Password" />
+          <button>Click Here</button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
